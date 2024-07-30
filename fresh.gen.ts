6344723badd71896d11159cbd7e92724f6ec9e6f
Rodrigo Araujo <rodrigo.araujo@corebiz.ag>
2024-07-30 15:00:58 +0000

    Release staging environment with 2 changes made by Rodrigo Araujo

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,14 +3,16 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $_app from "./routes/_app.tsx";
-
+import * as $Form from "./islands/Form.ts";
 import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
     "./routes/_app.tsx": $_app,
   },
-  islands: {},
+  islands: {
+    "./islands/Form.ts": $Form,
+  },
   baseUrl: import.meta.url,
 } satisfies Manifest;
 
